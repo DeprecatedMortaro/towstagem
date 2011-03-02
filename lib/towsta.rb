f45a179922a8e6b8f610537fcd044d2d5abfe6b7
Mortaro <mortaro@nChan.(none)>
2011-03-02 10:42:14 -0300

    gem seems like a gem now

--- lib/towsta.rb
+++ lib/towsta.rb
@@ -1,5 +1,5 @@
 require File.expand_path('../towsta/tree', __FILE__)
-#require File.expand_path('../towsta/vertical', __FILE__)
+require File.expand_path('../towsta/vertical', __FILE__)
 require File.expand_path('../towsta/synchronizer', __FILE__)
 
 module Towsta
