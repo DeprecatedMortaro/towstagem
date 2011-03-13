c07e8fdf0cff13befc09b74220e387046adc2a9d
Mortaro <mortaro@nChan.(none)>
2011-03-12 22:45:27 -0300

    demo test

--- lib/towsta.rb
+++ lib/towsta.rb
@@ -4,6 +4,3 @@ require File.expand_path('../towsta/synchronizer', __FILE__)
 
 module Towsta
 end
-
-Towsta::Synchronizer.new :secret => 'sGeQOCDIUUx9BrcFvRP5cqCqPA05ZGdVkq5H694rV8w9'
-puts "Synchronized with a demo towsta site"
