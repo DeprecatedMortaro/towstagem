e20376743fa7ddf7a00ea1e884705c6b74ea631b
Mortaro <mortaro@nChan.(none)>
2011-03-16 01:41:28 -0300

    reverse api

--- lib/towsta/vertical.rb
+++ lib/towsta/vertical.rb
@@ -15,7 +15,6 @@ module Towsta
 
         class << self
           attr_accessor :all, :tree, :attributes
-          @attributes = []
         end
 
         args[:slices].each do |attr, kind|
@@ -23,7 +22,6 @@ module Towsta
           eval "def #{attr}; #{Vertical.parse_get attr, kind}; end;"
           eval "def self.find_by_#{attr} value; self.all.each {|e| return e if e.#{attr} == value}; nil; end;"
           eval "def self.find_all_by_#{attr} value; found = []; self.all.each {|e| found << e if e.#{attr} == value}; found; end;"
-          @attributes << eval(attr.to_s)
         end
 
         def self.count
