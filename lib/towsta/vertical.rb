313de7990c47f652e987c67d48405917a40391db
Mortaro <mortaro@nChan.(none)>
2011-04-28 03:44:26 -0300

    testando list

--- lib/towsta/vertical.rb
+++ lib/towsta/vertical.rb
@@ -115,7 +115,7 @@ module Towsta
         end
 
         def self.add_occurrence occurrence
-          self.send(:define_method, "self.occurrences_for_#{occurrence[:name].downcase}") do
+          self.send(:"self.define_method", "occurrences_for_#{occurrence[:name].downcase}") do
             eval occurrence[:items].inspect
           end
         end
