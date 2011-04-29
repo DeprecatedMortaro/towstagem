24a7b82b21e47c67316c7d3b7bb8b5915044ed4d
Mortaro <mortaro@nChan.(none)>
2011-04-29 03:01:07 -0300

    <3

--- lib/towsta/synchronizer.rb
+++ lib/towsta/synchronizer.rb
@@ -22,7 +22,7 @@ module Towsta
       end
       begin
         uri = "/synchronizers/#{@secret}/#{Time.now.to_i}/export.json"
-        uri += "?query=#{CGI::escape(@params.to_json)}" if @params
+        puts "?query=#{CGI::escape(@params.to_json)}" if @params
         puts uri
         Net::HTTP.start("manager.towsta.com"){|http| @json = http.get(uri).body}
         puts 'Synchronized with towsta...'
