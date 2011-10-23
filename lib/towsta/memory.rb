2ef91c2d647801b92ef9eb4e965889f1ed427eb1
Mortaro <mortaro@UbuntoSan.(none)>
2011-10-23 20:59:23 -0200

    teste

--- lib/towsta/memory.rb
+++ lib/towsta/memory.rb
@@ -1,3 +1,19 @@
-class Memory
+require 'dalli'
+set :cache, Dalli::Client.new
 
+class Memory
+  def recover params
+    params.each do |key, value|
+      puts "#{key} => #{value}"
+    end
+    Towsta::Synchronizer.new :secret => $towsta_secret, :path => $towsta_path, :params => params
+  end
+end
+
+get '/' do
+  if settings.cache.get('teste')
+    haml settings.cache.get('teste')
+  else
+    haml Teste.first.id
+  end
 end
