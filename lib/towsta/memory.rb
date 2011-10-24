45fbb18427e1adddf3017866e1bcaa2c19dd182b
Mortaro <mortaro@UbuntoSan.(none)>
2011-10-23 22:57:56 -0200

    teste

--- lib/towsta/memory.rb
+++ lib/towsta/memory.rb
@@ -1,4 +1,6 @@
 require 'dalli'
+require 'digest/md5'
+
 set :cache, Dalli::Client.new
 
 module Towsta
@@ -9,19 +11,20 @@ module Towsta
       if settings.cache.get(params.to_s)
         puts 'USANDO CACHEEEEEEEEEEEEEEEEEEEEEEEEEE'
         params.each do |key, value|
-          cache_string =  "testetetas"#"#{key} #{value}"
-          puts cache_string
-          Object.const_set key.to_s, settings.cache.get(cache_string)
+          Object.const_set key.to_s, settings.cache.get(Memory.md5(key,value))
         end
       else
         puts 'CRIANDO CACHEEEEEEEEEEEEEEEEE'
         Towsta::Synchronizer.new :secret => $towsta_secret, :path => $towsta_path, :params => params
         settings.cache.set(params.inspect.to_s,true)
         params.each do |key, value|
-          cache_string =  "testetetas"#"#{key} #{value}"
-          settings.cache.set(cache_string,eval(key.to_s))
+          settings.cache.set(Memory.md5(key,value),eval(key.to_s))
         end
       end
+    end
+
+    def self.md5 key, value
+      digest = Digest::MD5.hexdigest(key+value)
     end
 
   end
