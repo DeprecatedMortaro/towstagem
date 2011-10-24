6795a88ae4e9346f041209c6d9a02696013092d6
Mortaro <mortaro@UbuntoSan.(none)>
2011-10-24 00:13:47 -0200

    teste

--- lib/towsta/memory.rb
+++ lib/towsta/memory.rb
@@ -10,10 +10,10 @@ module Towsta
     def self.recover params
       md5 = Memory.md5(params)
       if settings.cache.get(md5)
-        puts 'USANDO CACHEEEEEEEEEEEEEEEEEEEEEEEEEE'
+        puts 'Using cache to Towst'
         Towsta::Synchronizer.new :path => $towsta_path, :cache => settings.cache.get(md5)
       else
-        puts 'CRIANDO CACHEEEEEEEEEEEEEEEEE'
+        puts 'Creating cache'
         syn = Towsta::Synchronizer.new :secret => $towsta_secret, :path => $towsta_path, :params => params
         settings.cache.set(md5,syn.json)
       end
