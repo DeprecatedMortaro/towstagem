a5b4fcdc8b8e2bd97a77ea48d80c2cf085fdfbd9
Mortaro <mortaro@UbuntoSan.(none)>
2011-11-05 22:25:02 -0200

    reversal bitch

--- lib/towsta/sinatra_extension.rb
+++ lib/towsta/sinatra_extension.rb
@@ -3,3 +3,8 @@ helpers do
     haml "partials/_#{page}".to_sym
   end
 end
+
+get '/stylesheets/:name.css' do
+  content_type 'text/css', :charset => 'utf-8'
+  sass(:"stylesheets/#{params[:name]}", Compass.sass_engine_options)
+end
