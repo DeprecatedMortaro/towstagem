6e1fa630b1084a4d34d699d1b53043757096f6d9
root <root@UbuntoSan.(none)>
2011-11-23 20:53:46 -0200

    work work

--- lib/towsta/login.rb
+++ lib/towsta/login.rb
@@ -4,13 +4,13 @@ module Towsta
 
     def self.authenticate params
       response = Towsta::Synchronizer.authentication_request params
-      Sinatra::Sessionography.session[:current_user] = response[:id] if response[:status]
+      session[:current_user] = response[:id] if response[:status]
       response[:status]
     end
 
     def self.rescue
       return User.find(session[:current_user]) if defined? User
-      Sinatra::Sessionography.session[:current_user]
+      session[:current_user]
     end
 
 
