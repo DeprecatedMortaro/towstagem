f8d13922ef9c923a8ce2e12b4d12d3ea81b0d5d6
root <root@UbuntoSan.(none)>
2012-01-06 04:03:31 -0200

    go

--- lib/towsta/vertical.rb
+++ lib/towsta/vertical.rb
@@ -18,7 +18,9 @@ module Towsta
         include R18n::Helpers
 
         def i18n attr
-          self.send eval("t.models.#{self.class.to_s.downcase}.#{attr.to_s}").to_sym
+ #         return self.send eval("t.models.#{self.class.to_s.downcase}.#{attr.to_s}").to_sym if eval("t.models.#{self.class.to_s.downcase}.#{attr.to_s}.translated?")
+#          self.send :"#{attr.to_s}_#{t.code}"
+          self.send (t[:models][self.class.to_s.downcase.to_sym][attr] || "#{attr.to_s}_#{t.code}").to_sym
         end
 
         def to_mail
