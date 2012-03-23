c4cc976392e357fd1486c42fe4020094f5d3bc8f
unknown <Mortaro@dChan.(none)>
2012-03-23 13:26:52 -0300

    new production evn temp

--- lib/towsta/envs/production.rb
+++ lib/towsta/envs/production.rb
@@ -2,8 +2,14 @@ require 'dalli'
 set :cache, Dalli::Client.new
 require File.expand_path('../../memory', __FILE__)
 
-Towsta::Synchronizer.new params: {}, request: :structure
-
 def sync_with_towsta params={}
-  Towsta::Memory.recover(params).status
+  sync = Towsta::Synchronizer.new params: params, request: :all
+  (Dir["./controllers/*.rb"] + Dir["./models/*.rb"]).each {|file| load file}
+  sync.status
 end
+
+#Towsta::Synchronizer.new params: {}, request: :structure
+
+#def sync_with_towsta params={}
+#  Towsta::Memory.recover(params).status
+#end
